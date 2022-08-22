c1813a654846757a73adb4a0f7e761ce12893e11
Tyson Fritz <tysongfritz@gmail.com>
2022-08-22 08:56:16 -0700

    Use map of habitable planets to display results instead of using console.log inside the data feed.

--- exoplex.js
+++ exoplex.js
@@ -1,16 +1,39 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
-const planets = [];
+const min_insol = 0.36; //minimum insolation flux
+const max_insol = 1.11; //maximum insolation flux
+const max_prad = 1.6;   //maximum planetary radius
+var num_planets = 0;
+
+const habitablePlanets = [];
+
+function isHabitable(planet) {
+   return planet['koi_disposition'] === 'CONFIRMED'
+       && planet['koi_insol'] >= min_insol
+       && planet['koi_insol'] <= max_insol
+       && planet['koi_prad'] <= max_prad
+}
 
 fs.createReadStream('./data/koi_data.csv')
+   //pipe the stream to the CSV parser to break the data stream into rows
+   .pipe(parse({
+      comment: '#',
+      columns: true
+   }))
+   //process each row of data
    .on('data', (data) => {
-      planets.push(data);
+      num_planets++;
+      if(isHabitable(data)) {
+         habitablePlanets.push(data);
+      }
    })
    .on('error', (error) => {
       console.log(error);
    })
    .on('end', () => {
-      console.log(planets);
-      console.log('DONE');
+      habitablePlanets.map((planet) => {
+         console.log(`${planet['kepler_name']} may be habitable.`);
+      });
+      console.log(`HABITABLE PLANETS: ${habitablePlanets.length}/${num_planets}`);
    });
