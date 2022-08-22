2f841a5f0b4f853f42b2d5ccbff1aadde51c7f12
Tyson Fritz <tysongfritz@gmail.com>
2022-08-22 00:11:19 -0700

    use fs.createReadStream to load data.

--- exoplex.js
+++ exoplex.js
@@ -1,1 +1,16 @@
-console.log("Hello Galaxy");
+const { parse } = require('csv-parse');
+const fs = require('fs');
+
+const planets = [];
+
+fs.createReadStream('./data/koi_data.csv')
+   .on('data', (data) => {
+      planets.push(data);
+   })
+   .on('error', (error) => {
+      console.log(error);
+   })
+   .on('end', () => {
+      console.log(planets);
+      console.log('DONE');
+   });
