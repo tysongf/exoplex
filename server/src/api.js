5059ceff678915c38d5aa2251722a7a3eb79b11f
Tyson Fritz <tysongfritz@gmail.com>
2022-08-28 17:51:16 -0700

    added morgan logging

--- server/src/api.js
+++ server/src/api.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routers/planets.router');
 
 const api = express();
 
 api.use(cors({ origin: 'http://localhost:3000' }));
+api.use(morgan('combined'));
 api.use(express.json());
 api.use(planetsRouter);
 api.use(express.static(path.join(__dirname, '..', 'public')));
