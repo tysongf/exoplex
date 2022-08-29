f0bd4c36ac2c0356494573ee9b62f666630d1a64
Tyson Fritz <tysongfritz@gmail.com>
2022-08-28 22:02:38 -0700

    Validation on POST /launches route.

--- server/src/controllers/launches.controller.js
+++ server/src/controllers/launches.controller.js
@@ -8,7 +8,18 @@ function httpGetAllLaunches(req, res) {
 
 function httpPostLaunch(req, res) {
    const newLaunch = req.body;
+
+   if(!newLaunch.mission || !newLaunch.launchDate || !newLaunch.rocket
+      || !newLaunch.destination) {
+         return res.status(400).json({error: 'Missing required launch property'});
+      }
+
    newLaunch.launchDate = new Date(newLaunch.launchDate);
+
+   if(isNaN(newLaunch.launchDate)) {
+      return res.status(400).json({error: 'Invalid launch date.'});
+   }
+
    return res.status(201).json(
       launches.addNewLaunch(newLaunch)
    )
