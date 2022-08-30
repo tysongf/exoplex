96ce44f210753cb7accf89352e76af5fad177c82
Tyson Fritz <tysongfritz@gmail.com>
2022-08-29 22:33:45 -0700

    Test for posting new launch

--- server/src/routers/launches.test.js
+++ server/src/routers/launches.test.js
@@ -3,18 +3,52 @@ const api = require('../api');
 
 describe('Test GET /launches', () => {
    test('It should respond with 200 success', async () => {
-      const response = await request(api).get('/launches');
-      expect(response.statusCode).toBe(200);
+      const response = await request(api)
+      .get('/launches')
+      .expect('Content-Type', /json/)
+      .expect(200)
    })
 });
 
 describe('Test POST /launches', () => {
-   test('It should respond with 200 success', () => {
-      const response = 200;
+
+   const completeLaunchData = {
+      mission: 'USS Enterprise',
+      rocket: 'NCC 1701 D',
+      target: 'Kepler-186 f',
+      launchDate: 'April 1, 2030'
+   }
+   const completeLaunchDataWithoutDate = {
+      mission: 'USS Enterprise',
+      rocket: 'NCC 1701 D',
+      target: 'Kepler-186 f'
+   }
+
+   test('It should respond with 201 created', async () => {
+      const response = await request(api)
+      .post('/launches')
+      .send({
+         mission: 'USS Enterprise',
+         rocket: 'NCC 1701 D',
+         target: 'Kepler-186 f',
+         launchDate: 'April 1, 2030'
+      })
+      .expect('Content-Type', /json/)
+      .expect(201);
+
+      const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+      const responseDate = new Date(response.body.launchDate).valueOf();
+      expect(requestDate).toBe(responseDate);
+
+      expect(response.body).toMatchObject(completeLaunchDataWithoutDate);
+      expect(response)
+
    });
+
    test('It should catch missing required properties', () => {
 
    });
+
    test('It should catch invalid dates', () => {
 
    });
