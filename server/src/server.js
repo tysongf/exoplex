3ef8063fae68140797e81234fb5bc89f08a72d7a
Tyson Fritz <tysongfritz@gmail.com>
2022-08-30 11:14:52 -0700

    Utilize node cluster module to spawn multiple API instances depending on number of logical processors.

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,5 @@
+const os = require('os');
+const cluster = require('cluster');
 const http = require('http');
 const api = require('./api');
 const loadPlanetsData = require('./commands/load-planets');
@@ -7,9 +9,21 @@ const API_PORT = process.env.API_PORT || 8000;
 async function startServer() {
    await loadPlanetsData();
 
-   api_server.listen(API_PORT, () => {
-      console.log(`Listening on API_PORT: ${API_PORT} ...`);
-   });
+   //Master process spawns child processes to run API
+   if(cluster.isMaster) {
+      //Detect number of CPUs and create child processes
+      const NUM_CPUS = os.cpus().length;
+      console.log(`Server has ${NUM_CPUS} logical processors.`);
+      for(let cpu = 0; cpu < NUM_CPUS; cpu++) {
+         console.log(`Initializing process: ${cpu}`);
+         cluster.fork();
+      }
+   } else {
+      //Child processes run API server
+      api_server.listen(API_PORT, () => {
+         console.log(`Listening on API_PORT: ${API_PORT} ...`);
+      });
+   }
 }
 
 startServer();
