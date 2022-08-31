3df951ae48814180fad99d77824332856aea4095
Tyson Fritz <tysongfritz@gmail.com>
2022-08-31 12:14:53 -0700

    Filter out _id and __v from /planets route

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -8,7 +8,7 @@ async function upsertPlanet(planet) {
 }
 
 async function getAllPlanets() {
-   return await planets.find({});
+   return await planets.find({}, { '_id': 0, '__v': 0 });
 }
 
 module.exports = {
