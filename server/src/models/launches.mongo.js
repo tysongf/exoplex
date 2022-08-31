3b290e17f2e708496ff8368670d80af721da9e54
Tyson Fritz <tysongfritz@gmail.com>
2022-08-31 00:22:33 -0700

    Added launches mongo schema.

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const launchesSchema = new mongoose.Schema(
+   {
+      flightNumber: { type: Number, required: true },
+      launchDate: { type: Date, required: true },
+      mission: { type: String, required: true },
+      rocket: { type: String, required: true },
+      //target: { type: mongoose.ObjectId, ref: 'Planet' }
+      target: { type: String, required: true },
+      customers: { type: [String], required: true },
+      upcoming: { type: Boolean, required: true },
+      success: { type: Boolean, required: true, default: true }
+   }
+);
