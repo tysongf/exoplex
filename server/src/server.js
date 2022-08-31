49e6078624b630aed4850144de14623131bd1222
Tyson Fritz <tysongfritz@gmail.com>
2022-08-30 23:51:33 -0700

    Install mongoose and connect to mongo sandbox database.

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,28 @@
 const http = require('http');
+const mongoose = require('mongoose');
 const api = require('./api');
 const loadPlanetsData = require('./commands/load-planets');
 const api_server = http.createServer(api);
 const API_PORT = process.env.API_PORT || 8000;
 
+const DB_CLUSTER = 'cluster0';
+const DB_NAME = 'nasa';
+const DB_USER = 'nasa';
+const DB_PASSWORD = '1yxvWqcpXquiurnK';
+
+const MONGO_URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}.dtleewu.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose.connection.once('open', () => {
+   console.log("MongoDB Connection Open!");
+});
+
+mongoose.connection.on('error', (error) => {
+   console.error(error);
+});
+
 async function startServer() {
+   //Connect to Mongo DB using modern MongoDB settings
+   await mongoose.connect(MONGO_URL);
    await loadPlanetsData();
    api_server.listen(API_PORT, () => {
       console.log(`Listening on API_PORT: ${API_PORT} ...`);
