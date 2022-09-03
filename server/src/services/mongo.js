4ab5273ed60b0811c899967ecad669a5b7c56d96
Tyson Fritz <tysongfritz@gmail.com>
2022-09-03 15:09:38 -0700

    Combine Mongo .env settings into a single URL

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_CLUSTER = process.env.MONGO_CLUSTER;
-const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
-const MONGO_DB_USER = process.env.MONGO_DB_USER;
-const MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD;
-
-const MONGO_URL = `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@${MONGO_CLUSTER}.dtleewu.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority`;
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
    console.log("MongoDB Connection Open!");
